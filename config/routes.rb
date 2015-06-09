d05a21f94270613748cf8b040d4cb1fd1772004d
Austin Archibald <austin.archibald@gmail.com>
2015-06-09 00:05:06 -0600

    completed checkpoint 33, CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
+
+  root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
