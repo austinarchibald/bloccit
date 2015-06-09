dd4b65d8b8c596b6fc9549a3c31a319c6b8b7f09
Austin Archibald <austin.archibald@gmail.com>
2015-06-09 17:19:19 -0600

    assignment-40 completed

---
+++ app/controllers/summaries_controller.rb
@@ -0,0 +1,24 @@
+class SummariesController < ApplicationController
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post=Post.find(params[:post_id])
+    authorize @post
+    @summary= Summary.new
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post=Post.find(params[:post_id])
+    authorize @post
+    @summary= @post.build_summary(params.require(:summary).permit(:body))
+    if @summary.save
+      flash[:notice] = "Summary was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the summary. Please try again."
+      render :new
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index] do
+      resource :summary, except: [:show]
+    end
   end
 
   get 'about' => 'welcome#about'
