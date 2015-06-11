caceea4138cac835aa2603ed230ba1bc5fd8af9c
Austin Archibald <austin.archibald@gmail.com>
2015-06-11 00:00:04 -0600

    checkpoint-51 completed

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,30 @@
+class FavoritesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+    authorize favorite
+
+    if favorite.save
+      flash[:notice] = "Favorited."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Could not favorite. Please try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+
+    if favorite.destroy
+      flash[:success] = "Unfavorited."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Could not unfavorite. Please try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
+end
