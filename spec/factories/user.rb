5f8b3f6277dc801bae1153acb423733697fc1264
Austin Archibald <austin.archibald@gmail.com>
2015-06-11 15:08:44 -0600

    assignment 56 completed

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      after(:build) { |user| create(:post, user: user)}
+      after(:build) { |user| create(:comment, user: user)}
+    end
   end
 end
