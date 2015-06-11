17e9e3b9c08b2d7a69d88c4486cfe8a2b0e98c69
Austin Archibald <austin.archibald@gmail.com>
2015-06-11 00:45:33 -0600

    completed assignment 52, private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) } # This uses a lambda syntax (the arrow -> with a block) that is the proper way to pass a piece of code into a scope definition. This scope returns the equivalent of topic_collection.all or topic_collection.where(public: true), depending on the value of user. 
 end
